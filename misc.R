6c847d7e6e78ff45e119492a735291bfe76dda3c
Abubakar Athman <athman.abu@gmail.com>
2026-03-30 08:46:41 +0300

    inferential statistics

--- misc.R
+++ misc.R
@@ -1,3 +1,7 @@
 df <- read.xlsx(
   "./output/JMMI_fuel_prices_gasoline.xlsx"
 )
+
+county_vs_fuel <- df %>%
+  select(County, MSSMEB, MSSMEB.food.basket, mean_fuel_price) %>%
+  filter(County == "AweilCentre")
